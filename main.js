3bd768b2308f077341fd8fe5202bf6a3134c8996
Carlos Alcantara <12445620+cmalcantara@users.noreply.github.com>
2022-08-12 16:22:44 +0800

    intial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import newPlot from 'node_modules\plotly.js-dist\plotly.js';
+
+/*
+	Plotly.newPlot( TESTER, [{
+	x: [1, 2, 3, 4, 5],
+	y: [1, 2, 4, 8, 16] }], {
+	margin: { t: 0 } } );
+*/
+
+TESTER = document.getElementById('tester');
+
+var xArray = [50,60,70,80,90,100,110,120,130,140,150];
+var yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Define Data
+var data = [{
+  x: xArray,
+  y: yArray,
+  mode:"markers",
+  type:"scatter"
+}];
+
+// Define Layout
+var layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},
+  title: "House Prices vs. Size"
+};
+
+Plotly.newPlot(TESTER, data, layout);
