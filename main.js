7d4a5822048fe898b4bbd52de9e8c23bb3d82658
Carlos Alcantara <12445620+cmalcantara@users.noreply.github.com>
2022-08-20 04:43:56 +0800

    Added D, G, 2D Peaks

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ function usePlotly(xn, yn){
   //References
   //https://plotly.com/javascript/shapes/
 
+  //TODO
+  //
+
   // Define Data
   var data = [{
     x: xn,
     y: yn,
     type:"scatter"
   }];
+
+  var max = Math.max(...yn);
 
   // Define Layout
   var layout = {
@@ -23,8 +28,36 @@ function usePlotly(xn, yn){
             'x0': '1300',
             'y0': 0,
             'x1': '1400',
-            'y1': 100,
+            'y1': max,
             'fillcolor': 'yellow',
+            'opacity': 0.2,
+            'line': {
+                'width': 0,
+            }
+        },
+        {
+            'type': 'rect',
+            'xref': 'xaxis',
+            'yref': 'yaxis',
+            'x0': '1500',
+            'y0': 0,
+            'x1': '1700',
+            'y1': max,
+            'fillcolor': 'blue',
+            'opacity': 0.2,
+            'line': {
+                'width': 0,
+            }
+        },
+        {
+            'type': 'rect',
+            'xref': 'xaxis',
+            'yref': 'yaxis',
+            'x0': '2550',
+            'y0': 0,
+            'x1': '3000',
+            'y1': max,
+            'fillcolor': 'purple',
             'opacity': 0.2,
             'line': {
                 'width': 0,
@@ -43,26 +76,6 @@ function usePlotly(xn, yn){
   console.log("plotly plot success");
 }
 
-function useChartjs(xn, yn){
-  //CHART.JS
-  new Chart(document.getElementById('myChart'), {
-    type: 'line',
-    data: {
-      //labels: 'Frequency (cm$&{-1}$)',
-      labels: xn,
-      datasets: [{
-        label: 'Intensity (counts)',
-        data: yn,
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      }],
-    }
-  });
-  console.log("chartjs plot success");
-}
-
-
 // fetchs txt file, splts per line and splits columns to x & y
 fetch('https://raw.githubusercontent.com/meloon-de/tools/main/raman/2022-07-15/G-P-3s_D1_pos1.txt')
 .then(response => response.text())
@@ -70,6 +83,10 @@ fetch('https://raw.githubusercontent.com/meloon-de/tools/main/raman/2022-07-15/G
   var lines = text.split('\n');
   for(var line = 0; line < lines.length; line++){
       var lin = lines[line].split('\t');
+      //filters out NaN of file at the end which gives error to Math.max
+      if (isNaN(lin[1])) {
+        continue;
+      }
       x.push(lin[0]);
       y.push(lin[1]);
   }
@@ -87,7 +104,5 @@ fetch('https://raw.githubusercontent.com/meloon-de/tools/main/raman/2022-07-15/G
   console.log("convert to number success");
 
   usePlotly(xn,yn);
-  useChartjs(xn,yn);
-
 
 });
